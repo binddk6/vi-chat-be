62dd1ab41d0ff7201021bd77babfd900920edc97
deepak smartsense <deepak.kumar@smartsensesolutions.com>
2024-07-30 14:04:14 +0530

    updated

--- server.ts
+++ server.ts
@@ -6,13 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "https://vi-chat-git-development-binddk6s-projects.vercel.app/",
-      "https://vi-chat-inky.vercel.app/",
-      "https://vi-chat-binddk6s-projects.vercel.app/",
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
